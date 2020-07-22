9b0f4fad9156df4c0d932985a19cfec5b3cab899
IvanNess <chai@bk.ru>
2020-07-22 08:47:26 +0300

    build was added

---
+++ build/precache-manifest.e01e7c0a49b8d5fa1fa575bef771a562.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbaba1d5433eb0b2c92cc78f90d3495d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "750dc4951a5af9537c56",
+    "url": "/static/css/main.b8ea9a32.chunk.css"
+  },
+  {
+    "revision": "45d2c8115ecd1783f54d",
+    "url": "/static/js/2.f5e446ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f5e446ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "750dc4951a5af9537c56",
+    "url": "/static/js/main.15cb11e3.chunk.js"
+  },
+  {
+    "revision": "1bd51f528316b920c919",
+    "url": "/static/js/runtime-main.3adcbeab.js"
+  }
+]);
